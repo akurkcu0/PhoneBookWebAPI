b1cf5621ec14d41399a6c73bb7ba73eb81efba64
akurkcu0 <kurkcuabdurrahman@hotmail.com>
2023-01-12 16:07:05 +0300

    CRUD operations for Reports

---
+++ PhoneBookWebAPI/Controllers/ReportsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhoneBookWebAPI.Models.Context;
+
+namespace PhoneBookWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly PhoneBookDbContext _context;
+
+        public ReportsController(PhoneBookDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var locations = _context.contactInformations.GroupBy(p => p.Location).Select(s => s.Key).ToList();
+            var result = from location in locations
+                         select new
+                         {
+                             Location = location,
+                             NumberOfPeople = _context.contactInformations.Where(p => p.Location == location).Count(),
+                             NumberOfPhone = _context.contactInformations.Where(p => p.Location == location && p.PhoneNumber != "").Count(),
+                         };
+
+            return Ok(result.OrderBy(p => p.NumberOfPeople).ToList());
+        }
+    }
+}
