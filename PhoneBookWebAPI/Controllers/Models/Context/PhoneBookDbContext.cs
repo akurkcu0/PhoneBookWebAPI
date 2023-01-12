dead10097faa17a7adbef164070fbe6bef053f7a
akurkcu0 <kurkcuabdurrahman@hotmail.com>
2023-01-12 12:39:30 +0300

    Add project files.

---
+++ PhoneBookWebAPI/Controllers/Models/Context/PhoneBookDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PhoneBookWebAPI.Controllers.Models.Context
+{
+    public class PhoneBookDbContext : DbContext
+    {
+        public PhoneBookDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
